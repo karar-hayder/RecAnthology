b012746dba76349e96956afd3b1aa0d39a6d35ea
Karar <krarhayder45@gmail.com>
2024-07-19 19:27:24 +0300

    deleted wrong phone column

--- users/models.py
+++ users/models.py
@@ -2,17 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, phone_number, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
 
         if not email:
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email, phone_number=phone_number, **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, phone_number, password=None, **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
@@ -21,7 +21,7 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self.create_user(email, phone_number, password, **extra_fields)
+        return self.create_user(email, password, **extra_fields)
     
 class CustomUser(AbstractUser):
     first_name = models.CharField(max_length=30,null=False,blank=False)
