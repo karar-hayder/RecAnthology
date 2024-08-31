11ccda804d57f9624d7920b04f737916eaef185e
Karar <krarhayder45@gmail.com>
2024-08-31 03:28:16 +0300

    Added static dirs and production static

--- RecAnthology/settings.py
+++ RecAnthology/settings.py
@@ -146,6 +146,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,"static")
+STATIC_ROOT = os.path.join(BASE_DIR,"production_static")
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'static'),
+)
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
