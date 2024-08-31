79510aac1994588784e9f5752d9e7495648876c7
Karar <krarhayder45@gmail.com>
2024-08-31 15:08:07 +0300

    Added home page and books explore with base.html and their urls

--- Books/urls.py
+++ Books/urls.py
@@ -1,26 +1,24 @@
 from django.urls import path
-from .views import (
-    IndexView,
-    AllGenres,
-    AllBooks,
-    FilterBooks,
-    GetBook,
-    CreateBook,
-    CreateGenre,
-    PublicRecommendBooks,
-    PrivateRecommendBooks
-)
+from . import API_views
+from . import views
+
+
 from users.views import RateBook
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    path('api/books/genres/', AllGenres.as_view(), name='books-all-genres'),
-    path('api/books/', AllBooks.as_view(), name='books-all'),
-    path('api/books/<int:id_query>/', GetBook.as_view(), name='book-detail'),
-    path('api/books/filter/', FilterBooks.as_view(), name='books-filter'),
-    path('api/books/genre/create/', CreateGenre.as_view(), name='books-create-genre'),
-    path('api/books/create/', CreateBook.as_view(), name='books-create'),
-    path('api/books/recommend/public/', PublicRecommendBooks.as_view(), name='books-recommend-public'),
-    path('api/books/recommend/private/', PrivateRecommendBooks.as_view(), name='books-recommend-private'),
+    #### Main ####
+    path("", views.MainPage.as_view(), name="home"),
+    path("books/explore/", views.ExploreBooksPage.as_view(), name="explore books"),
+
+    #### API ####
+    # path('', API_views.IndexView.as_view(), name='index'),
+    path('api/books/genres/', API_views.AllGenres.as_view(), name='books-all-genres'),
+    path('api/books/', API_views.AllBooks.as_view(), name='books-all'),
+    path('api/books/<int:id_query>/', API_views.GetBook.as_view(), name='book-detail'),
+    path('api/books/filter/', API_views.FilterBooks.as_view(), name='books-filter'),
+    path('api/books/genre/create/', API_views.CreateGenre.as_view(), name='books-create-genre'),
+    path('api/books/create/', API_views.CreateBook.as_view(), name='books-create'),
+    path('api/books/recommend/public/', API_views.PublicRecommendBooks.as_view(), name='books-recommend-public'),
+    path('api/books/recommend/private/', API_views.PrivateRecommendBooks.as_view(), name='books-recommend-private'),
     path('api/books/rate/', RateBook.as_view(), name='books-rate'),
 ]
--- Books/views.py
+++ Books/views.py
@@ -1,8 +1,40 @@
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from django.views.generic.edit import CreateView
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Count
+from django.core.cache import cache
 
+from .models import Book,Genre
 
 class MainPage(TemplateView):
-    pass
+    template_name = 'home.html'
+
+
+class ExploreBooksPage(TemplateView):
+    template_name = 'books/explore_books.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        most_liked_books = cache.get('most_liked_books',None)
+        recently_added_books = cache.get('recently_added_books',None)
+        genres_books = cache.get('genre_books',None)
+
+        if not genres_books:
+            recently_added_books = Book.objects.order_by('-pk')[:10]
+            most_liked_books = Book.objects.order_by('-likedPercent')[:10]
+            genres = Genre.objects.annotate(books_count=Count('books')).order_by('-books_count')[:10]
+            genres_books = {}
+            for genre in genres:
+                genres_books[genre] = genre.books.all()[:10]
+
+            cache.set('recently_added_books',recently_added_books,60*60)
+            cache.set('most_liked_books',most_liked_books,60*60)
+            cache.set('genre_books',genres_books,60*60)
+
+        context['recently_added_books'] = recently_added_books
+        context['most_liked_books'] = most_liked_books
+        context['genre_books'] = genres_books
+
+        return context
