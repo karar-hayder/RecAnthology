6ae534d8c58ce1c27efd0b9926add56fc4ea2e77
Karar <krarhayder45@gmail.com>
2024-08-31 15:09:25 +0300

    Added static and media url for debug

--- RecAnthology/urls.py
+++ RecAnthology/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path(settings.ADMIN_PAGE_URL, admin.site.urls),
@@ -24,3 +25,7 @@ urlpatterns = [
     path('', include('moviesNshows.urls')),
     path('users/', include('users.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
