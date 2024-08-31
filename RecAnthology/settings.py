e4216cb91eb0845000ce1ef3c7849fcbbfb8c2d6
Karar <krarhayder45@gmail.com>
2024-08-31 14:15:42 +0300

    Added Media root and url

--- RecAnthology/settings.py
+++ RecAnthology/settings.py
@@ -151,5 +151,8 @@ STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static'),
 )
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
