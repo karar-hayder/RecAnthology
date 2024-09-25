c311343b4f2dc94813bb107026d631c6498230b9
Karar <krarhayder45@gmail.com>
2024-09-25 18:51:12 +0300

    Clearing cache on start

--- Books/apps.py
+++ Books/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
-
+from django.core.cache import cache
 
 class BooksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Books'
+    def ready(self) -> None:
+        cache.clear()
+        return super().ready()
