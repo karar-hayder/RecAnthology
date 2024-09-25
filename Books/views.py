7860bd7e752dfbaabadfc3b40638bacd581de744
Karar <krarhayder45@gmail.com>
2024-09-25 18:51:28 +0300

    Better books query

--- Books/views.py
+++ Books/views.py
@@ -16,7 +16,7 @@ class ExploreBooksPage(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        
+
         most_liked_books = cache.get('most_liked_books',None)
         recently_added_books = cache.get('recently_added_books',None)
         genres_books = cache.get('genre_books',None)
@@ -25,9 +25,12 @@ class ExploreBooksPage(TemplateView):
             recently_added_books = Book.objects.order_by('-pk')[:10]
             most_liked_books = Book.objects.order_by('-likedPercent')[:10]
             genres = Genre.objects.annotate(books_count=Count('books')).order_by('-books_count')[:10]
+            
+            books = Book.objects.filter(genre__in=genres).distinct()
             genres_books = {}
             for genre in genres:
-                genres_books[genre] = genre.books.all()[:10]
+                genre_books = books.filter(genre=genre)[:10]
+                genres_books[genre] = genre_books
 
             cache.set('recently_added_books',recently_added_books,60*60)
             cache.set('most_liked_books',most_liked_books,60*60)
